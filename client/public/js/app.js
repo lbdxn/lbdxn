245a2ccb57fd7ccf971a186875053512e8d5ca56
lbdxn <138144146+lbdxn@users.noreply.github.com>
2024-09-27 17:40:26 +0800

    Delete client directory

--- client/public/js/app.js
+++
@@ -1,7 +0,0 @@
-import { setupTokenRefresh } from './api.service.js';
-
-// ... 其他初始化代码
-
-setupTokenRefresh();
-
-// ... 其他代码
--- client/src/index.js
+++
@@ -1,4 +0,0 @@
-import './css/main.css';
-import './js/project-form.js';
-import './js/dump-detail.js';
-// 导入其他必要的JS文件
